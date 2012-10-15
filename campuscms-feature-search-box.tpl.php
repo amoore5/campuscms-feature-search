e171387b6a5a473ea0f584a2a096ffedac60f420
Adam Moore <amoore5@ucmerced.edu>
2012-10-14 21:26:19 -0700

    Change id into class and add an additional class.

--- campuscms-feature-search-box.tpl.php
+++ campuscms-feature-search-box.tpl.php
@@ -1,6 +1,6 @@
 <form action="http://google.com/cse" id="cse-search-box">
   <input name="cx" type="hidden" value="016184560432393330487:cyxse6qvvgw" />
   <input name="ie" type="hidden" value="UTF-8" />
-  <input id="search-box" name="q" size="23" type="text" />
-  <input name="sa" type="submit" value="Search" />
+  <input class="search-box" name="q" size="23" type="text" />
+  <input class="submit-button" name="sa" type="submit" value="Search" />
 </form>
