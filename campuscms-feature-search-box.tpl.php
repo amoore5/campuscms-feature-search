a914ed827b5f63c55de8f0af452aaa3b1c17f2ce
Adam Moore <amoore5@ucmerced.edu>
2012-10-14 21:21:37 -0700

    Add an id for the search box.

--- campuscms-feature-search-box.tpl.php
+++ campuscms-feature-search-box.tpl.php
@@ -1,6 +1,6 @@
 <form action="http://google.com/cse" id="cse-search-box">
   <input name="cx" type="hidden" value="016184560432393330487:cyxse6qvvgw" />
   <input name="ie" type="hidden" value="UTF-8" />
-  <input name="q" size="23" type="text" />
+  <input id="search-box" name="q" size="23" type="text" />
   <input name="sa" type="submit" value="Search" />
 </form>
